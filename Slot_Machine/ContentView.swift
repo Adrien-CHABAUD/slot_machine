f91fe06713bd2368885e4f1101764b13ec3b3919
Adrien-CHABAUD <adrien.chabaud@hotmail.com>
2022-01-20 22:31:52 -0500

    Initial Commit

---
+++ Slot_Machine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot_Machine
+//
+//  Created by Adrien CHABAUD on 2022-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slot_Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot_Machine
+//
+//  Created by Adrien CHABAUD on 2022-01-20.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
